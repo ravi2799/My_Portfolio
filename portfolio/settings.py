559623a902fedfcc6917a3a5d9a03904895e5471
Raviraj savaliya <68138958+ravi2799@users.noreply.github.com>
2020-07-14 16:02:23 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e(7zwftg_z4)okxle6kemn-!i30^6*y%0npb*@^)a$os&rf6wn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['ravirajsavaliya.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['ravirajweb.herokuapp.com','localhost']
 
 
 # Application definition
